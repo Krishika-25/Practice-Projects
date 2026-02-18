28a3fd0958b8a31052b9a3b24833f46436b4a83b
Krishika-dev <krishikad25@gmail.com>
2026-02-18 23:58:12 +0530

    Add loading and error handling for weather search

--- Weather-app/script.js
+++ Weather-app/script.js
@@ -1,11 +1,8 @@
 let cityInput = document.getElementById("city-input");
 let searchBtn = document.getElementById("SearchBtn");
-let apiKey = "bd6a33449ea1083bef77eccc464f58c6";
+let apiKey = "ff71243e917cd70febd745ca27c7b0fe";
 
-
-let cityName = document.querySelector(".city");
-let temp = document.querySelector(".temp");
-let condition = document.querySelector(".condition");
+let weatherBox = document.querySelector(".weather-box");
 
 searchBtn.addEventListener("click", async function () {
     let city = cityInput.value.trim();
@@ -14,13 +11,19 @@ searchBtn.addEventListener("click", async function () {
         return;
     }
 
-
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    console.log(url);
+    weatherBox.innerHTML = `<p>Loading...</p>`;
     let response = await fetch(url);
+    if (!response.ok) {
+        weatherBox.innerHTML = `<p>City not found ❌</p>`;
+        return;
+    }
     let data = await response.json();
-    cityName.innerText = data.name;
-    temp.innerText = `${data.main.temp}°C`;
 
-    condition.innerText = data.weather[0].main;
+    weatherBox.innerHTML = `
+    <h2 class="city">${data.name}</h2>
+    <p class="temp">${Math.round(data.main.temp)}°C</p>
+    <p class="condition">${data.weather[0].main}</p>
+`;
 })
+
