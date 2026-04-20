c4427c9eee792dde08301e18429e2bb02699990e
Krishika-dev <krishikad25@gmail.com>
2026-04-20 23:14:01 +0530

    Built the ui for quiz app and defined the basic structure

---
+++ Quiz-app/script.js
@@ -0,0 +1,20 @@
+let start = document.querySelector(".start-btn");
+let questions = [
+    {
+        question: "What is capital of Scotland?",
+        options: ["Edinburgh", "Berlin", "Paris", "Rome"],
+        answer: "Edinburgh"
+    },
+    {
+        question: "What is capital of Germany?",
+        options: ["Madrid", "Berlin", "Lisbon", "Vienna"],
+        answer: "Berlin"
+    },
+    {
+        question: "What is capital of France?",
+        options: ["Paris", "Rome", "Berlin", "London"],
+        answer: "Paris"
+    }
+];
+let index = 0;
+
