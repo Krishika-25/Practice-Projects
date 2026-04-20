4f6e063a5e5e0b1543bcc6790c6e61b273d87a7b
Krishika-dev <krishikad25@gmail.com>
2026-04-20 23:38:49 +0530

    Added 3 questions for the quiz using array in js

--- Quiz-app/script.js
+++ Quiz-app/script.js
@@ -1,4 +1,7 @@
 let start = document.querySelector(".start-btn");
+let quiz = document.querySelector(".quiz");
+let options = document.querySelector(".options");
+
 let questions = [
     {
         question: "What is capital of Scotland?",
@@ -16,5 +19,68 @@ let questions = [
         answer: "Paris"
     }
 ];
-let index = 0;
 
+let index = 0;
+let correct = "";
+
+function showQuestion() {
+
+    quiz.innerHTML = "";
+    options.innerHTML = "";
+
+    let q = questions[index];
+
+    let li = document.createElement("li");
+    li.textContent = q.question;
+    quiz.appendChild(li);
+
+    q.options.forEach(opt => {
+        let option = document.createElement("li");
+        option.textContent = opt;
+        options.appendChild(option);
+    });
+
+    correct = q.answer;
+}
+
+start.addEventListener("click", function () {
+    index = 0;
+    showQuestion();
+});
+
+options.addEventListener("click", (e) => {
+
+    if (e.target.tagName === "LI") {
+
+        let all = document.querySelectorAll(".options li");
+
+        all.forEach(a => {
+            a.classList.remove("correct");
+            a.classList.remove("wrong");
+        });
+
+        if (e.target.textContent === correct) {
+            e.target.classList.add("correct");
+        } else {
+            e.target.classList.add("wrong");
+
+            all.forEach(a => {
+                if (a.textContent === correct) {
+                    a.classList.add("correct");
+                }
+            });
+        }
+
+        setTimeout(() => {
+            index++;
+
+            if (index < questions.length) {
+                showQuestion();
+            } else {
+                quiz.innerHTML = "<li>Quiz Finished</li>";
+                options.innerHTML = "";
+            }
+
+        }, 3000);
+    }
+});
